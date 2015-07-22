7422e1b36b1373b41c86c4d91da3d261836af185
Torsten Friebe <torsten.friebe@gmail.com>
2015-07-23 00:00:17 +0200

    Drop all vehicle demo classes and replaced with simple App and AppTest classes

--- examples/versioneye-plugin-example/src/main/java/de/crowdcode/vehicle/dao/EngineDao.java
+++
@@ -1,7 +0,0 @@
-package de.crowdcode.vehicle.dao;
-
-import de.crowdcode.vehicle.domain.Engine;
-
-public interface EngineDao extends EntityDao<Engine>{
-
-}
--- examples/versioneye-plugin-example/src/main/java/de/crowdcode/vehicle/dao/ManufacturerDao.java
+++
@@ -1,9 +0,0 @@
-package de.crowdcode.vehicle.dao;
-
-import de.crowdcode.vehicle.domain.Manufacturer;
-
-public interface ManufacturerDao extends EntityDao<Manufacturer> {
-    
-    Manufacturer findManufacturerByName(String name);
-
-}
