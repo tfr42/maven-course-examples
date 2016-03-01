6165d65d495b486da27bfdb38fdd34b9a09dfdc5
Torsten Friebe <torsten.friebe@gmail.com>
2016-03-01 22:09:38 +0100

    Added output of system properties to App and jar plugin with manifest

--- examples/docker-example/src/main/java/course/maven/App.java
+++ examples/docker-example/src/main/java/course/maven/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello, world!" );
+        System.out.println( "Running with Java SE " + System.getProperty("java.version")
+        + " by " + System.getProperty("java.vendor"));
+        System.out.println( "on " + System.getProperty("os.name")
+                + " " + System.getProperty("os.version"));
+
+        System.out.println("JRE: " + System.getProperty("java.vm.version")
+        + " " + System.getProperty("java.runtime.name"));
+        System.out.println("JVM: " + System.getProperty("java.vm.name"));
     }
 }
