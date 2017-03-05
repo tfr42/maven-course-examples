bd7103edc82ca3e1490c281d8d77c7f395eb1190
Torsten Friebe <torsten.friebe@gmail.com>
2017-03-06 00:46:19 +0100

    Added integration test and build-helper plugin to allocate network port for integration test

---
+++ solutions/mvn.04.solution-webapp/src/test/java/course/maven/SimpleServletIT.java
@@ -0,0 +1,32 @@
+package course.maven;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by tf on 09.02.17.
+ */
+public class SimpleServletIT {
+
+    @Test
+    public void verifyThatServletResponseIsValid() throws IOException {
+        String port = System.getProperty("test.server.port")==null?"8080":System.getProperty("test.server.port");
+        URL url = new URL("http://localhost:"+port+"/simple-webapp/simple");
+        System.out.println(url.toExternalForm());
+        URLConnection conn = url.openConnection();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+            String pageText = reader.lines().collect(Collectors.joining("\n"));
+            assertThat(pageText, containsString("SimpleServlet"));
+        }
+    }
+}
