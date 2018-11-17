bdd7b6e807954bd213de43635184866d9b57ed2f
Torsten Friebe <torsten.friebe@gmail.com>
2018-11-17 10:05:10 +0100

    Upgraded unit tests to JUnit 5 and set version to SNAPSHOT

--- examples/simple-release/src/main/java/maven/course/release/App.java
+++ examples/simple-release/src/main/java/maven/course/release/App.java
@@ -14,7 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello, world!" );
         displayPackageDetails(Package.getPackage("maven.course.release"));
         try {
             displayManifestEntries();
